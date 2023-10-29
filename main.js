8f1c4412b64db1e99c02ea22c1cd727338a4b58b
irina777-create <71390012+irina777-create@users.noreply.github.com>
2023-10-29 21:15:35 +0200

    изменения main.js
    Signed-off-by: irina777-create <71390012+irina777-create@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-//Створити масив, довжину та елементи якого задає користувач.
-const array = [5,34,-89,34,66];
+//Знайти суму та кількість позитивних елементів.
+const array = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+const posArray  = [];
+let sumElements = 0;
+array.forEach(element){
+    if (element > 0) {
+        posArray.push(element);
+        sumElements += element;
+    }
+}
+
+const numOfElements = posArray.length;
+console.log('кількість позитивних єлементів', numOfElements);
+console.log('сума позитивних єлементів', sumElements);
+
 //Відсортувати масив за зростанням.//
 const max= Math. max(array);
 const index0fMaxNumber = array.index0f(MaxNumber);
@@ -10,5 +23,33 @@ const min= Math. min(array);
 const index0fMinNumber = array.index0f (minNumber);
 console.Log('minimum number', minNumber);
 console.log ('index of minimum',index0fMinNumber);
+//Визначити кількість негативних елементів.
+const negativeArray = array.filter(function (item){return item ‹ 0});
+console.log('Кількість негативних елементів = ${negativeArray.length}.' );
+//Знайти кількість непарних позитивних елементів.
+const unpositivetlement = positarray.tiiter(tunction (item) (return item 2 % != 0);
+console.log(' Кількість непарних позитивних елементів = ${unpositiveElement.length}. ');
+//Знайти кількість парних позитивних елементів.
+const positivetlement = positarray.tiiter(tunction (item) (return item 2 % === 0);
+console.log(' Кількість непарних позитивних елементів = ${positiveElement.length}. ');
+//Знайти суму парних позитивних елементів.
+const sumElements = PositiveElements.reduce (sum);
+console.log( 'Сума парних елементів масиву = ${sumElements}.' );
+//Знайти суму непарних позитивних елементів.
+const sumUnElements = unPositiveElement.reduce(sum);
+console.log(  'Сума непарних позитивних елементів = ${sumUnElements}');
+//Знайти добуток позитивних елементів.
+const mulPositiceElements = positArray.reduce(accumulator);
+console.log( 'добуток позитивних елементів = >{accumulator' );
+//Знайти найбільший серед елементів масиву, остальні обнулити.
+array. forEach((element, index){
+    if (element!== maxNumber) {
+        array[index] = 0;
+    }
+
+}
+console.log(maxNumber);
+
+
 
 
