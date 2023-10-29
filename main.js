74e77dae83cfbddedf26be59d7c3893411bde353
irina777-create <71390012+irina777-create@users.noreply.github.com>
2023-10-29 19:45:01 +0200

    изменения main.js
    Signed-off-by: irina777-create <71390012+irina777-create@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,14 @@
+//Створити масив, довжину та елементи якого задає користувач.
+const array = [5,34,-89,34,66];
+//Відсортувати масив за зростанням.//
+const max= Math. max(array);
+const index0fMaxNumber = array.index0f(MaxNumber);
+console.Log('maximum number', maxNumber);
+console.log ('index of maximum',index0fMaxNumber);
+// Знайти мінімальний елемент масиву та його порядковий номер.
+const min= Math. min(array);
+const index0fMinNumber = array.index0f (minNumber);
+console.Log('minimum number', minNumber);
+console.log ('index of minimum',index0fMinNumber);
+
+
